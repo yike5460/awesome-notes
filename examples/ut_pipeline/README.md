ac7aa4f8db305848c7626d07f6e9301fb1c4202d
yike5460 <yike5460@163.com>
2024-03-11 08:31:19 +0000

    feat: roll out lint

--- examples/ut_pipeline/README.md
+++ examples/ut_pipeline/README.md
@@ -1,1 +1,16 @@
 # Sample Unit Test Pipeline
+
+## Execute the following commands to run the pipeline
+```
+# poetry install
+
+# make format
+
+# make lint
+
+# make test
+```
+
+## ruff sample output
+
+[image](./image/ruff.png)
--- examples/ut_pipeline/ut_sample/messy_code.py
+++ examples/ut_pipeline/ut_sample/messy_code.py
@@ -11,47 +11,55 @@ This file includes several issues that ruff can automatically fix, such as:
     - Long lines that exceed the recommended length
     - Unused function definitions (unused_function and messy_function)
 """
+
+
 class ExampleClass:
-  def __init__(self, name, value):
-    self.name=name
-    self.value=value
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
 
-  def say_hello( self ):
-    greeting = "Hello, " + self.name+"!"
-    print(greeting)
+    def say_hello(self):
+        greeting = "Hello, " + self.name + "!"
+        print(greeting)
 
-def calculate_sum(x,y):
-    sum=x+y
+
+def calculate_sum(x, y):
+    sum = x + y
     return sum
+
 
 def unused_function():
     pass
 
-# This is a poorly formatted function
-def messy_function(    ):
-  x=  { 'a':37,'b':42,
 
-  'c':927}
-  y = 'hello '    'world'
-  z = 'hello '+'world'
-  a = 'hello {}'.format('world')
-  class Foo  (     object  ):
-    def f    (self   ):
-        return       37*-+2
-    def g(self, x,y=42):
-        return y
-def f  (   a ) :
-  return      10
+# This is a poorly formatted function
+def messy_function():
+    x = {"a": 37, "b": 42, "c": 927}
+    y = "hello " "world"
+    z = "hello " + "world"
+    a = "hello {}".format("world")
+
+    class Foo(object):
+        def f(self):
+            return 37 * -+2
+
+        def g(self, x, y=42):
+            return y
+
+
+def f(a):
+    return 10
+
 
 # Unused variables
 unused_var1 = 123
 unused_var2 = "Hello, World!"
 
 # Long line
-long_line = 'This is a really really really really really really really really really really really long line.'
+long_line = "This is a really really really really really really really really really really really long line."
 
-if __name__=="__main__":
-  example = ExampleClass('John Doe', 42)
-  example.say_hello()
-  result = calculate_sum(5,10)
-  print("Sum is:",result)
+if __name__ == "__main__":
+    example = ExampleClass("John Doe", 42)
+    example.say_hello()
+    result = calculate_sum(5, 10)
+    print("Sum is:", result)
