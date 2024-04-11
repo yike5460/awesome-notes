66db100526c3d9c092025b90e6d2f58b0e58dd1f
yike5460 <yike5460@163.com>
2024-04-11 14:19:21 +0000

    feat: add card step

--- examples/RAG/rag-portal/src/app/page.tsx
+++ examples/RAG/rag-portal/src/app/page.tsx
@@ -1,113 +1,174 @@
 import Image from "next/image";
 
-export default function Home() {
+import * as React from "react";
+import Cards from "@cloudscape-design/components/cards";
+import Box from "@cloudscape-design/components/box";
+import SpaceBetween from "@cloudscape-design/components/space-between";
+import Button from "@cloudscape-design/components/button";
+import TextFilter from "@cloudscape-design/components/text-filter";
+import Header from "@cloudscape-design/components/header";
+import Pagination from "@cloudscape-design/components/pagination";
+import CollectionPreferences from "@cloudscape-design/components/collection-preferences";
+import Link from "@cloudscape-design/components/link";
+
+export default () => {
+  const [
+    selectedItems,
+    setSelectedItems
+  ] = React.useState([{ name: "Item 2" }]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Cards
+      onSelectionChange={({ detail }) =>
+        setSelectedItems(detail?.selectedItems ?? [])
+      }
+      selectedItems={selectedItems}
+      ariaLabels={{
+        itemSelectionLabel: (e, t) => `select ${t.name}`,
+        selectionGroupLabel: "Item selection"
+      }}
+      cardDefinition={{
+        header: item => (
+          <Link href="#" fontSize="heading-m">
+            {item.name}
+          </Link>
+        ),
+        sections: [
+          {
+            id: "description",
+            header: "Description",
+            content: item => item.description
+          },
+          {
+            id: "type",
+            header: "Type",
+            content: item => item.type
+          },
+          {
+            id: "size",
+            header: "Size",
+            content: item => item.size
+          }
+        ]
+      }}
+      cardsPerRow={[
+        { cards: 1 },
+        { minWidth: 500, cards: 2 }
+      ]}
+      items={[
+        {
+          name: "Item 1",
+          alt: "First",
+          description: "This is the first item",
+          type: "1A",
+          size: "Small"
+        },
+        {
+          name: "Item 2",
+          alt: "Second",
+          description: "This is the second item",
+          type: "1B",
+          size: "Large"
+        },
+        {
+          name: "Item 3",
+          alt: "Third",
+          description: "This is the third item",
+          type: "1A",
+          size: "Large"
+        },
+        {
+          name: "Item 4",
+          alt: "Fourth",
+          description: "This is the fourth item",
+          type: "2A",
+          size: "Small"
+        },
+        {
+          name: "Item 5",
+          alt: "Fifth",
+          description: "This is the fifth item",
+          type: "2A",
+          size: "Large"
+        },
+        {
+          name: "Item 6",
+          alt: "Sixth",
+          description: "This is the sixth item",
+          type: "1A",
+          size: "Small"
+        }
+      ]}
+      loadingText="Loading resources"
+      selectionType="multi"
+      trackBy="name"
+      visibleSections={["description", "type", "size"]}
+      empty={
+        <Box
+          margin={{ vertical: "xs" }}
+          textAlign="center"
+          color="inherit"
+        >
+          <SpaceBetween size="m">
+            <b>No resources</b>
+            <Button>Create resource</Button>
+          </SpaceBetween>
+        </Box>
+      }
+      filter={
+        <TextFilter filteringPlaceholder="Find resources" />
+      }
+      header={
+        <Header
+          counter={
+            selectedItems?.length
+              ? "(" + selectedItems.length + "/10)"
+              : "(10)"
+          }
+        >
+          Common cards with selection
+        </Header>
+      }
+      pagination={
+        <Pagination currentPageIndex={1} pagesCount={2} />
+      }
+      preferences={
+        <CollectionPreferences
+          title="Preferences"
+          confirmLabel="Confirm"
+          cancelLabel="Cancel"
+          preferences={{
+            pageSize: 6,
+            visibleContent: [
+              "description",
+              "type",
+              "size"
+            ]
+          }}
+          pageSizePreference={{
+            title: "Page size",
+            options: [
+              { value: 6, label: "6 resources" },
+              { value: 12, label: "12 resources" }
+            ]
+          }}
+          visibleContentPreference={{
+            title: "Select visible content",
+            options: [
+              {
+                label: "Main distribution properties",
+                options: [
+                  {
+                    id: "description",
+                    label: "Description"
+                  },
+                  { id: "type", label: "Type" },
+                  { id: "size", label: "Size" }
+                ]
+              }
+            ]
+          }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      }
+    />
   );
 }
